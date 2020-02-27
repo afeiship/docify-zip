b4b99198866b47e68d841d5435a1c3fb72f773d4
afeiship <1290657123@qq.com>
2020-02-28 00:52:10 +0800

    add: initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# docify-zip
+> Zip with thor cli for docify.
+
+## installation
+```shell
+npm i @feizheng/docify-zip
+```
+
+## resources
+- https://ma.ttias.be/create-a-password-protected-zip-file-on-mac-osx/
---
+++ src/index.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require "thor"
+require "fileutils"
+require "tty-spinner"
+
+README_FILE = 'https://raw.githubusercontent.com/afeiship/docify-zip/master/files/README.txt'
+
+module ThorCli
+  class DocifyZip < Thor
+    desc "zip NAME", "Zip with thor cli for docify."
+    option :force, :type => :boolean
+    def zip(filename, suffix, password)
+      name = File.basename(filename,'.*')
+      spinner = TTY::Spinner.new("[:spinner] Zipping #{name}...", format: :spin)
+      spinner.auto_spin
+      # cache readme
+      if !File.exist?("/tmp/README.txt") || options[:force]
+        system "https_proxy=http://127.0.0.1:9090 wget -q --directory-prefix=/tmp #{README_FILE}"
+      end
+
+      system "zip -jq #{name}#{suffix}.zip #{filename} /tmp/README.txt --password #{password}"
+      spinner.success('(successful)')
+    end
+
+    def self.exit_on_failure?
+      false
+    end
+  end
+end
+
+ThorCli::DocifyZip.start(ARGV)
+
+# ruby src/index.rb hello boilerplate-book-notes
