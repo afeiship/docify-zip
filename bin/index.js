1dffa0ca04c21ff7f4540c8c0570a585d75c5e35
afeiship <1290657123@qq.com>
2021-10-07 22:09:12 +0800

    chore: basename

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 const { Command } = require('commander');
 const chalk = require('chalk');
+const fs = require('fs');
+const path = require('path');
 
 // next packages:
 require('@jswork/next');
@@ -14,10 +16,18 @@ const QUICK_URL = 'https://github.91chifun.workers.dev';
 const FILES_URL = `${QUICK_URL}/https://github.com/afeiship/docify-zip/archive/refs/heads/master.zip`;
 const TARGET_FILES = '/tmp/docify-zip-master/files/';
 
+const basename = (inFilename) => {
+  const isDir = fs.lstatSync(inFilename).isDirectory();
+  if (isDir) return path.basename(inFilename);
+  const extname = path.extname(inFilename);
+  return path.basename(inFilename, extname);
+};
+
 program.version(version);
 
 program
   .option('-d, --debug', 'only show cmds, but not clean.')
+  .option('-f, --filename <string>', 'The zip target filename.')
   .option('-p, --password <string>', 'The zip password.')
   .option('-s, --src <string>', 'source filepath.', './src')
   .parse(process.argv);
@@ -36,11 +46,11 @@ nx.declare({
         url: FILES_URL,
         filename: `/tmp/docify-zip.zip`
       });
-
+      const name = basename(program.filename);
       const command = program.password ? ` --password ${password}` : '';
 
       exec(`cd /tmp && unzip docify-zip.zip`);
-      exec(`zip -jq '${name}${suffix}.zip' ${filename} ${TARGET_FILES}/* ${command}`);
+      exec(`zip -jq '${name}${suffix}.zip' ${program.filename} ${TARGET_FILES}/* ${command}`);
       // /tmp/docify-zip-master/files/
       // console.log(chalk.green('🚗 hello cli.'), program.src, program.debug);
     }
