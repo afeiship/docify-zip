bdcf86ea05cade895b2e51c411d7dd5f9ba60881
afeiship <1290657123@qq.com>
2021-10-07 22:01:49 +0800

    refactor: for nodejs version

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'tty-spinner'
-gem 'fileutils'
-gem 'thor'
---
+++ bin/index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+const { Command } = require('commander');
+const chalk = require('chalk');
+
+// next packages:
+require('@jswork/next');
+require('@jswork/next-absolute-package');
+require('@jswork/next-node-downfile');
+
+const { version } = nx.absolutePackage();
+const program = new Command();
+const exec = require('child_process').execSync;
+const QUICK_URL = 'https://github.91chifun.workers.dev';
+const FILES_URL = `${QUICK_URL}/https://github.com/afeiship/docify-zip/archive/refs/heads/master.zip`;
+const TARGET_FILES = '/tmp/docify-zip-master/files/';
+
+program.version(version);
+
+program
+  .option('-d, --debug', 'only show cmds, but not clean.')
+  .option('-p, --password <string>', 'The zip password.')
+  .option('-s, --src <string>', 'source filepath.', './src')
+  .parse(process.argv);
+
+nx.declare({
+  statics: {
+    init() {
+      const app = new this();
+      app.start();
+    }
+  },
+  methods: {
+    init() {},
+    async start() {
+      await nx.nodeDownfile({
+        url: FILES_URL,
+        filename: `/tmp/docify-zip.zip`
+      });
+
+      const command = program.password ? ` --password ${password}` : '';
+
+      exec(`cd /tmp && unzip docify-zip.zip`);
+      exec(`zip -jq '${name}${suffix}.zip' ${filename} ${TARGET_FILES}/* ${command}`);
+      // /tmp/docify-zip-master/files/
+      // console.log(chalk.green('🚗 hello cli.'), program.src, program.debug);
+    }
+  }
+});
